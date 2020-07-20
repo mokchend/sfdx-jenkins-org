e53bdea77dbdf8b9441c6dd5666f01445149a894
chendamok-gmail <chenda.mok@gmail.com>
2020-07-20 22:48:56 +0200

    Update Jenkins file

--- env-devcicd.groovy
+++ env-devcicd.groovy
@@ -1,9 +1,9 @@
 env.DB_URL="hello"
 env.DB_URL2="hello2"
 //--------------------------------------- Salesforce Org Environment variable
-env.SF_CONSUMER_KEY=3MVG9Kip4IKAZQEW8SIBzWoPWNyBHn8lKJuXVntaUke8zaC2caLhEvPrCfv3_SBfKSfLxatZnXeiIJBsuKH9z
-env.SF_USERNAME=chenda.mok@gmail.com.devcicd
-env.SERVER_KEY_CREDENTIALS_ID=923F1ACEE196C740B02F2F899BB3D63A483D60CE75114A9999BB528EB9DF2A9D
-env.DEPLOYDIR=src
-env.TEST_LEVEL=RunLocalTests
-env.SF_INSTANCE_URL=https://login.salesforce.com
+env.SF_CONSUMER_KEY="3MVG9Kip4IKAZQEW8SIBzWoPWNyBHn8lKJuXVntaUke8zaC2caLhEvPrCfv3_SBfKSfLxatZnXeiIJBsuKH9z"
+env.SF_USERNAME="chenda.mok@gmail.com.devcicd"
+env.SERVER_KEY_CREDENTIALS_ID="923F1ACEE196C740B02F2F899BB3D63A483D60CE75114A9999BB528EB9DF2A9D"
+env.DEPLOYDIR="src"
+env.TEST_LEVEL="RunLocalTests"
+env.SF_INSTANCE_URL="https://login.salesforce.com"
